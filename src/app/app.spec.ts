5235346e29c05ef0ae8d44c1e6c563494de6dcb5
neovenio <shirinsana78@gmail.com>
2026-08-30 16:29:43 +0200

    deployment test: formatting fix

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,8 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, this is my Firebase project');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, this is my Firebase project',
+    );
   });
 });
